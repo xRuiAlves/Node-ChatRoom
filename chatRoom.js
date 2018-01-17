f8db1eafdaf01d30a1b52f04d1a69e4fe41369ee
xRuiAlves <up201606746@fe.up.pt>
2018-01-17 23:00:40 +0000

    Finished Connect Users nicknames listing and added page styling

--- chatRoom.js
+++ chatRoom.js
@@ -9,6 +9,7 @@ var io = require('socket.io').listen(server);
 // List of all connected users
 connectedUsers = [];
 
+app.use(express.static('public'));
 
 // Declare which port server is listening too
 server.listen(3000);
@@ -22,7 +23,6 @@ app.get('/' , application);
 io.sockets.on('connection' , clientConnected);
 
 
-
 //////////////////
 // Event Handlers
 
@@ -33,18 +33,26 @@ function application(request , response){
 
 function clientConnected(socket){
     // New connected user
-    connectedUsers.push(socket);
-    updateConnectedUsers();
+    socket.nickname = '';   // Not 'logged in' yet , not nickname associated
 
     // Client disconnected
     socket.on('disconnect' , function (){
-        // Remove the user that disconnected
-        connectedUsers.splice(connectedUsers.indexOf(socket),1);
-        updateConnectedUsers();
+        // Remove the user that disconnected, if it was 'logged in'
+        if (socket.nickname != ''){
+            connectedUsers.splice(connectedUsers.indexOf(socket),1);
+            updateConnectedUsers();
+        }
     });
 
     // Broadcast the message to all clients
     socket.on('send message' , sendMessage);
+
+    // Client chose nickname
+    socket.on('new client nickname', function(nickname){
+        socket.nickname = nickname;
+        connectedUsers.push(socket);
+        updateConnectedUsers();
+    });
 }
 
 function sendMessage(message){
@@ -55,8 +63,7 @@ function updateConnectedUsers(){
     nicknames = [];
 
     for (i=0 ; i<connectedUsers.length ; i++){
-        nicknames.push(connectedUsers[i].id);
-        console.log(connectedUsers[i].id);
+        nicknames.push(connectedUsers[i].nickname);
     }
 
     io.sockets.emit('update current users' , nicknames);
