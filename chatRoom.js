884436774636cc4c20562ab89f44d4d5ad2d739a
xRuiAlves <up201606746@fe.up.pt>
2018-01-18 18:20:07 +0000

    Added message auto scroll and improved styling

--- chatRoom.js
+++ chatRoom.js
@@ -9,6 +9,7 @@ var io = require('socket.io').listen(server);
 // List of all connected users
 connectedUsers = [];
 
+// Express is using the 'public' folder (for accessing style.css)
 app.use(express.static('public'));
 
 // Declare which port server is listening too
@@ -40,7 +41,7 @@ function clientConnected(socket){
         // Remove the user that disconnected, if it was 'logged in'
         if (socket.nickname != ''){
             // Notify the other Users
-            io.sockets.emit('system message' , 'User <b>' + socket.nickname + '</b> has left the chat-room.');
+            socket.broadcast.emit('system message' , 'User <b>' + socket.nickname + '</b> has left the chat-room.');
 
             connectedUsers.splice(connectedUsers.indexOf(socket),1);
             updateConnectedUsers();
@@ -52,22 +53,29 @@ function clientConnected(socket){
 
     // Client chose nickname
     socket.on('new client nickname', function(nickname){
+        // Add the new client to the sockets list
         socket.nickname = nickname;
         connectedUsers.push(socket);
+
+        // Notify other clients only that this new clients has joined the chat-room
+        socket.broadcast.emit('system message' , 'User <b>' + socket.nickname + '</b> has joined the chat-room.');
+
         updateConnectedUsers();
     });
 }
 
 function sendMessage(message){
+    // Broadcast the message to all clients
     io.sockets.emit('new message' , '<b>' + message.nickname + '</b>:&nbsp&nbsp' + message.content);
 }
 
 function updateConnectedUsers(){
+    // Make a list of all the clients nicknames
     nicknames = [];
-
     for (i=0 ; i<connectedUsers.length ; i++){
         nicknames.push(connectedUsers[i].nickname);
     }
 
+    // Tell all the clients which clients are currently in the chat-room
     io.sockets.emit('update current users' , nicknames);
 }
